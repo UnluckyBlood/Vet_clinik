1db8bfa642247e8508c2e7513137cbaba93cf21c
Victoria-Pletneva <pletnevavika07@mail.ru>
2025-05-05 16:50:33 +0300

    fonts and home_user.html/css and SVG image

--- vet-clinic/db.js
+++ vet-clinic/db.js
@@ -1,11 +1,12 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres',         // Имя пользователя в PostgreSQL
-    host: 'localhost',        // Сервер базы данных (оставь localhost, если локально)
-    database: 'vet_db',       // Название БД (проверь в pgAdmin)
-    password: '915', // Пароль пользователя PostgreSQL
-    port: 5432,               // Порт (обычно 5432)
+    user: 'postgres',
+    host: 'localhost',
+    database: 'vetclinik',
+    password: '07052004',
+    port: 5432,
 });
 
 module.exports = pool;
+
